68a6164ad202c653fc433881e460d49c7dbd7af9
EC2 Default User <ec2-user@ip-172-31-47-200.ap-south-1.compute.internal>
2026-05-06 14:37:08 +0000

    Made changes to main and variables files

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,13 +1,12 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
 ########################
 # VPC Module
 ########################
 module "vpc" {
-  source       = "git::https://github.com/mfasi7863/terraform-modules.git//vpc?ref=main"
-  cluster_name = "my-eks-cluster"
+  source = "git::https://github.com/mfasi7863/terraform-modules.git//vpc?ref=main"
+
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 }
 
 ########################
@@ -15,18 +14,17 @@ module "vpc" {
 ########################
 module "ecr" {
   source    = "git::https://github.com/mfasi7863/terraform-modules.git//ecr?ref=main"
-  repo_name = "my-app"
+  repo_name = var.repo_name
 }
 
 ########################
 # EKS Module
 ########################
 module "eks" {
-  source          = "git::https://github.com/mfasi7863/terraform-modules.git//eks?ref=main"
-  cluster_name    = "my-eks-cluster"
+  source = "git::https://github.com/mfasi7863/terraform-modules.git//eks?ref=main"
 
-  subnet_ids      = concat(module.vpc.public_subnets, module.vpc.private_subnets)
-  node_subnet_ids = module.vpc.private_subnets
+  cluster_name = var.cluster_name
+  subnet_ids   = concat(module.vpc.public_subnets, module.vpc.private_subnets)
 
   depends_on = [module.vpc]
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,4 +1,35 @@
-variable "aws_region" {
-  description = "AWS region for infrastructure"
+variable "cluster_name" {
+  description = "EKS cluster name"
   type        = string
+  default     = "my-eks-cluster"
+}
+
+variable "repo_name" {
+  description = "ECR repository name"
+  type        = string
+  default     = "my-app"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
 }
